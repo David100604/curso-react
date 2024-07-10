14963cbd4596fa8df3f4c273aa716ea0cf7d3189
David <114587007+David100604@users.noreply.github.com>
2024-07-10 13:26:57 -0300

    Finalização aula 1 + início aula 2

--- aula01/script.js
+++ aula01/script.js
@@ -16,3 +16,54 @@
 // Escreve no documento/página
 // document.write("<h1>Aprendendo JavaScript</h1>");
 // document.write("<img src='http://google.com/google.jpg' />");
+
+
+// funcao entrar
+/*
+function entrar(){
+
+    var area = document.getElementById('area');
+    var texto = prompt('Digite seu nome: ');
+
+    if(texto == '' || texto == null){
+        alert('Dgite seu nome novamente')
+        area.innerHTML = 'Bem vindo ... ';
+    } else{
+        area.innerHTML = 'Bem vindo ' +  texto;
+    }
+}
+*/
+
+
+// funcao entrar2
+/*
+function entrar2(nome){
+
+    var area = document.getElementById('area2');
+    var texto = prompt('Digite seu sobrenome: ');
+
+    area.innerHTML = nome + ' ' +  texto;
+}
+*/
+
+// Arrays
+// var lista = ["Teste", 2, "David", "Vitor", 5]; // cria a lista
+
+// lista.length; // retorna o tamanho da lista
+// lista[0]; // retorna o valor armazenado no índice passado como argumento ("Teste")
+// lista.indexOf("David"); // retorna o índice do valor passado (2), caso passe um valor que não existe, ele devolve -1
+// lista.join(" / "); // Junta a lista em uma string, passando como argumento o que estará entre cada item
+// lista.pop(); // Remove o último item da lista
+// lista.shift(); // Remove o primeiro item da lista
+// lista.push(20) // Adiciona item a lista
+
+// Editar um item que você não sabe o índice
+// console.log(lista.indexOf(2)); // para exibir o índice no console
+// lista[1] = 10; // define o novo valor para o índice encontrado
+
+// Verifica se um item está na lista
+// if(lista.indexOf("Teste") > -1){
+//     alert("Esse item está na lista")
+// } else {
+//     alert("Item não encontrado")
+// }
---
+++ aula02/script.js
@@ -0,0 +1,50 @@
+
+// LOOPS
+
+// while = enquanto.
+
+// x = 0;
+
+// while(x < 10){
+//     document.write("<br/> O valor do X é: " + x);
+
+//     x++;
+// }
+
+
+// document.write("<br><br><br> O X está valendo: " + x + "<br><br>")
+
+// // for = para
+
+// for(a = 0; a < x; a++){
+//     document.write("<br/> O valor do A é: " + a);
+// }
+
+// MENU
+// document.write("<br>Escolha seu pedido: <br>")
+// document.write("<br>0 - Sorvete | 1 Suco <br>")
+// document.write("<br>2 - Coca | 3 Água <br><br>")
+
+// SWITCH
+
+// function pedir(){
+//     x = prompt("O que deseja pedir? ");
+
+//     switch(x){
+//         case "0":
+//             alert("Você pediu um sorvete :)");
+//             break;
+//         case "1": 
+//             alert("Você pediu um suco :)");
+//             break;
+//         case "2": 
+//             alert("Você pediu uma coca :)");
+//             break;
+//         case "3": 
+//             alert("Você pediu uma água :)");
+//             break;
+//         default:
+//             alert("Não temos essa opção :(");
+//             break;
+//     }
+// }
