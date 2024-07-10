8c2639cd37c54ccf5e146ff42dcef0f79d8d8284
David <114587007+David100604@users.noreply.github.com>
2024-07-10 09:10:27 -0300

    Primeiras aulas

---
+++ aula01/script.js
@@ -0,0 +1,18 @@
+// variável do tipo str
+// var nome = "David"; 
+
+/*
+    Comentário
+    de múltiplas
+    linhas
+*/
+
+// Armazena o input do usuário na variável idade (retorna sempre uma str)
+// var idade = prompt("Digite sua idade: ");
+
+// document se refere à página do navegador
+// window se refere a janela em si
+
+// Escreve no documento/página
+// document.write("<h1>Aprendendo JavaScript</h1>");
+// document.write("<img src='http://google.com/google.jpg' />");
